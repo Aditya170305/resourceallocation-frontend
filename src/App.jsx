b8339adb2f60d89e02eaff24563314760107672c
Aditya170305 <adityagupta.bpl08@gmail.com>
2026-06-02 12:43:00 +0530

    Fix file name

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Login from "./pages/Login"; // New import
 import FacultyDashboard from "./pages/FacultyDashboard";
 import ResourceCalendar from "./pages/ResourceCalendar";
 import ShowResources from "./pages/ShowResources";
-import MyRequests from "./pages/MyRequests"; // ✅ import
+import MyRequests from "./pages/MyRequests"; //
 import HodDashboard from "./pages/HodDashboard";
 import HodAllBookings from "./pages/HodAllBookings";
 import HodResources from "./pages/HodResources";
@@ -29,7 +29,7 @@ function App() {
         <Route path="/faculty-dashboard" element={<FacultyDashboard />} />
         <Route path="/resource-calendar" element={<ResourceCalendar />} />
         <Route path ="/show-resources" element={<ShowResources />} />
-        <Route path="/my-requests" element={<MyRequests />} /> // ✅ add this
+        <Route path="/my-requests" element={<MyRequests />} />git
         <Route path="/hod-dashboard" element={<HodDashboard />} />
         <Route path="/hod-all-bookings" element={<HodAllBookings />} />
         <Route path="/hod-resources" element={<HodResources />} />
